9a88bd7ebd4f82f88a221656f5397d90af55c1f1
Pedro Augusto Lopes Gomes <pedroaugustolopesgomes@gmail.com>
2024-08-26 10:06:18 -0300

    chore: first commit

---
+++ src/main/java/com/example/API_JavaEvents/ApiJavaEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_JavaEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/API_JavaEvents/ApiJavaEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_JavaEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
